53ebd7014d1d752b03d1740f58d2749dd2e402bd
sharif-islam <abmsharif555@gmaile.com>
2021-07-12 03:17:59 +0600

    recipe application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    margin: "10px auto",
+    alignItems: "center",
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const appId = "de583c55";
+  const appKey = "fd1b16ebc8334c0d027274eeaec8290e";
+  const [recipe, setRecipe] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
+
+  const getRecipe = async () => {
+    const response = await axios.get(
+      `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`
+    );
+    setRecipe(response.data.hits);
+  };
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const updateQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Paper onSubmit={updateQuery} component="form" className={classes.root}>
+        <InputBase
+          type="text"
+          value={search}
+          onChange={updateSearch}
+          className={classes.input}
+          placeholder="Search For Recipe"
+          inputProps={{ "aria-label": "search For Recipe" }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
         >
-          Learn React
-        </a>
-      </header>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      {/* <form onSubmit={updateQuery}>
+        <input type="text" value={search} onChange={updateSearch} />
+        <button type="submit">Search</button>
+      </form> */}
+      <div>
+        <Grid container>
+          {recipe.map((pd) => (
+            <Grid item xs={3}>
+              <Recipe key={pd.recipe.label} recipe={pd}></Recipe>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 325,
+    margin: "15px 0",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+const Recipe = (props) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  //console.log(props);
+  const { label, calories, image, ingredients } = props.recipe.recipe;
+  // console.log(ingredients);
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardHeader title={label} subheader={calories} />
+        <CardMedia className={classes.media} image={image} title={label} />
+        <CardActions disableSpacing>
+          <div>Ingredients</div>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {ingredients.map((ingredient) => (
+              <Typography paragraph>{ingredient.text}</Typography>
+            ))}
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
+};
+
+export default Recipe;
